efaf32caceab8d644b67d0fe9af14b692f35328d
jbardon <bardonjeremy1@gmail.com>
2015-11-15 17:45:47 +0100

    Bank client refactoring, test no save

--- client/java/src/main/java/org/alma/services/client/Client.java
+++ client/java/src/main/java/org/alma/services/client/Client.java
@@ -1,30 +1,33 @@
 package org.alma.services.client;
 
 import org.apache.axis2.AxisFault;
+
 import org.apache.ws.axis2.BankStub;
-import org.apache.ws.axis2.BankStub.GetCredit;
-import org.apache.ws.axis2.BankStub.GetCreditResponse;
+
+import org.apache.ws.axis2.BankStub.*;
 
 import java.rmi.RemoteException;
 
 public class Client {
 	public static void main(String[] args) {
 		try {
-			//create service stub with the endpoint of the sercer
-			BankStub stub=new BankStub("http://192.168.2.2:9763/services/Bank/");
-			//calling for addition operation in the service
-			GetCredit add=new GetCredit();
-			
-			//consume the service using stub and get the response
-			GetCreditResponse response=stub.getCredit(add);
+			BankStub stub = new BankStub("http://192.168.2.2:9763/services/Bank/");
 
-			System.out.println("Reponse from Axis2 Service: "+response.get_return());
-		} catch (AxisFault e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			GetCredit credit1 = new GetCredit();
+			GetCreditResponse response1 = stub.getCredit(credit1);
+			System.out.println("Reponse from Axis2 Service: " + response1.get_return());
+
+			MakeOnlinePayment payment = new MakeOnlinePayment();
+			payment.setArgs0(10);
+			MakeOnlinePaymentResponse response2 = stub.makeOnlinePayment(payment);
+			System.out.println("Reponse from Axis2 Service: " + response2.get_return());			
+
+			GetCredit credit2 = new GetCredit();
+			GetCreditResponse response3 = stub.getCredit(credit2);
+			System.out.println("Reponse from Axis2 Service: " + response3.get_return());			
+
+		} 
+		catch (AxisFault e) { e.printStackTrace(); } 
+		catch (RemoteException e) {	e.printStackTrace(); }
 	}
 }
